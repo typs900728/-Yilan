57a024175adbdec47df82bf51de9a8a8884cab84
名字typs900728 <typs900728@gmail.com>
2021-08-24 03:03:26 +0800

    地圖

--- view.js
+++ view.js
@@ -1,87 +1,98 @@
 function changeUmipark() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-頭城交流道下-省道臺2庚線-省道臺2線。<br><br>大眾運輸：</br>搭臺鐵至龜山站下，步行約15分鐘。";
     img.src = "/view/a.jpg"
     title.innerHTML = "北關海潮公園"
     text.innerHTML = "北關公園又稱蘭城公園，地處臺灣東北角沿岸，位於頭城鎮更新里，南端設有南關，遙相呼應，成為進出蘭陽的門戶，是一座景觀豐富的海濱公園。<br>北關原為滿清時期的一座關卡，嘉慶年間清政府鑑於此地地形為防止盜匪進入宜蘭的最佳關隘，而奏請設立駐兵關卡。<br>公園裡立有兩尊古砲，據說是清代嘉慶年間所鑄造的古物。公園內有經過千萬年沖刷而成的單面山、豆腐岩、小海岬等自然石岩景觀，堪稱東海岸特殊的景觀。<br>在巨石與山勢對峙中，並設有階梯、步道和觀海涼亭，園中處處展現海濱特有奇景。<br>由觀海臺上南望，可遠眺龜山島及蘭陽平原，近看則驚濤拍岸，氣象萬千，蘭陽八景中的「北關海潮」即指此地。觀海亭可清楚眺望龜山島、外澳等海上景致，以及北關一帶全景。<br>站在觀海亭上，遠眺龜嶼佳景，近觀驚濤拍岸，氣象萬千、景色壯麗，尤其在漲潮或日出時，更是走訪北關的最佳時刻，巨石嶙峋峨立海岸，海濤洶湧，驚濤裂岸，一波波浪濤湧進石腹之中，潮聲迴盪，不絕於耳。"
 }
 function changeForestpark() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-宜蘭交流道下-省道臺7線-省道臺7甲線-專用公路宜專1線。<br><br>大眾運輸：<br>搭臺鐵至宜蘭站或羅東站下-轉搭國光客運1750（往太平山）至太平山站下。";
     img.src = "/view/b.jpg"
     title.innerHTML = "太平山國家森林遊樂區"
     text.innerHTML = "位於大同鄉的太平山國家森林遊樂區，面積達一萬兩千餘頃，全區有南湖大山山系、三星山山系、大霸尖山支脈，以盛產紅檜、扁柏聞名，曾與阿里山、八仙山並列為臺灣三大林場之一。<br>其四季風貌皆有所不同，春暖花開時五彩繽紛；秋高氣爽時，日出雲海等風情最讓人難以忘懷。本區海拔高差甚大，兼有亞熱帶至寒帶氣候，各林帶樹種繁雜，材質優美。 區內的景觀包括鳩之澤溫泉（原仁澤溫泉）、森林樂園、森林浴步道、野生動物保護區、翠峰湖等。<br>另外由於高低差的關係，具有四季紅葉植物變化的景致，每到秋季，火紅的楓樹為太平山帶來艷麗的色彩。此外山櫻花、梅、杏、太平山白櫻花等亦隨四季展現不同的風采，十分迷人。"
 }
 function changeGuishanIsland() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-頭城交流道下-省道臺2庚線-省道臺2線-青雲路三段390巷1弄-港墘路-烏石港-搭船至龜山島。<br><br>大眾運輸：<br>1.搭高鐵或臺鐵至臺北站下-轉搭國光客運1811（往羅東）、1812（往南方澳）至港口站下。<br>2.搭臺鐵至頭城站下-轉搭台灣好行綠18壯圍沙丘線至烏石港（蘭陽博物館）站下。<br>3.搭臺鐵至礁溪站下-轉搭台灣好行綠19宜蘭東北角海岸線至烏石港（蘭陽博物館）站下。";
     img.src = "/view/c.jpg"
     title.innerHTML = "龜山島"
     text.innerHTML = "龜山島位在宜蘭縣頭城鎮海岸以東約10公里處，是宜蘭縣縣屬島嶼之中最大的島嶼，也是臺灣目前尚存的活火山。無論從陸路或海路，只要看到龜山島，就知道宜蘭到了。<br>龜山島不但是宜蘭最著名的地標，也是宜蘭人的精神象徵。龜山島又名龜山嶼，為孤懸於海中之火山島嶼，因其型似浮龜而得名。<br>龜山島在地形上可分為龜首、龜甲、龜尾等三部份，其岩層主要是由安山岩質之熔岩流和火山碎屑岩互層所構成。自民國77年（西元1988年）解除戒嚴後，在宜蘭縣政府及地方民意代表多年積極努力之下，行政院於民國88年（西元1999年）將龜山島納為東北角暨宜蘭海岸國家風景區內。依龜山島人文及生態資源特性，規劃成為兼具知性及感性之海上生態公園，藉由遊客容納量的管制，減少旅遊環境衝突，並結合鄰近海域賞鯨活動，成為北部藍色公路熱門的旅遊景點。<br>龜湖又名龜尾潭，原來是一個淡水湖，後來島上居民引海水進湖，本希望成為一個漁港，但是居民每次築堤都被颱風吹垮而堵住，所以現在是鹹淡各半的湖泊。湖的對岸有一座觀音像，安詳的守護著龜尾潭及居民。龜尾位在龜山島的末端，為一道鵝卵石長灘。<br>因為受風向和潮水的影響，尾端會隨著季風而南北向移動，俗稱「神龜擺尾」。龜山島有一個海底世界，在熱騰騰的硫磺及強酸的環境之下，還有成千上萬的螃蟹生存著，成為一個罕見現象。<br>龜山島另一處著名的美景，是從島上駐軍營往前望去，山勢陡峭而且綠意盎然，山巒稜線分明，猶如一位美人仰天，還可以清楚的看見美人的形態，讓人讚嘆大自然的巧奪天工。"
 }
 function changeSouthernAustralia() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<>br國道5號-蘇澳交流道下-馬賽路-中山路二段至一段-蘇東中路-江夏路。<br><br>大眾運輸：<br>1.搭臺鐵至蘇澳站下。<br>2.搭臺鐵至羅東站下-轉搭國光客運（往南方澳）至南方澳或進安宮站下。"
     img.src = "/view/d.jpg"
     title.innerHTML = "南方澳"
     text.innerHTML = "位於蘇澳港南側，是臺灣沿海及近洋漁船的重要基地，漁獲量極豐，為臺灣三大漁業重鎮之一。此處有三個漁港及漁市，各依其功能、特色而有不同。<>br第一漁市每天約在下午2點左右，由漁會主持拍賣，第二漁市為每天約在下午3點到5點，由漁民們各自販售。第三漁港旁的漁港路，聚滿各式海鮮、特產品店，也是遊客聚集的地方。"
 }
 function changeBotanicalgarden() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-宜蘭交流道下-縣道191線-省道臺9線-省道臺7線-省道臺7丁線。";
     img.src = "/view/e.jpg"
     title.innerHTML = "福山植物園"
     text.innerHTML = "福山植物園為林業試驗所福山分所試驗林之一部分，園區有系統的蒐集、保存和栽培臺灣中低海拔木本植物，以期成為植物的博物館，提供林業研究、教學實習、生態環境教育及保存林木種源之基地。<br>園區展示區之配置，主要按恩格勒（Engler）氏分類的次序，設有裸子植物、離瓣花、合瓣花、杜鵑花、竹等區；又依植物特殊用途，設置特用植物區；另依生態特性設有水生、草本植物區，並保存天然林展示原生林相。"
 }
 function changewaterpark() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-羅東交流道下-縣道191線-省道臺7丙線。<br><br>大眾運輸：<br>搭臺鐵至羅東站下-轉搭台灣好行綠21冬山河線至冬山河親水公園站下。<br><br>無障礙交通資訊<br>大眾運輸：<br>搭臺鐵至羅東站下-轉搭台灣好行綠21冬山河線（部分低地板）至冬山河親水公園站下。<br><br>復康巴士：宜蘭縣政府社會處";
     img.src = "/view/f.jpg"
     title.innerHTML = "冬山河親水公園"
     text.innerHTML = "冬山河為宜蘭第五大河，流經羅東、冬山、五結等三鄉鎮，在全長24公里的冬山河流域中，經規劃為以運動、遊憩、休閒多元功能的河濱遊憩區，共分上、中、下三段水域，各自展現不同的遊憩風貌，期望能成為「戶外水態博物園」。<br>親水公園是冬山河最重要的遊憩據點，以重建人與水的自然倫理為訴求，模仿大自然造景並融入蘭陽本土色彩，如水上舞臺、五座圓錐形卵石丘及海面上的龜山島，不經意的將蘭陽地標引入園中。冬山河自古相傳有青龍及黃龍護岸，因此河畔用卵石築起高低起伏、彎曲有致的龍坡，分別鑲上青、藍和黃、橘的陶片，龍坡的階梯上，一方方有趣的嵌鑲畫，是宜蘭小朋友拼貼的傑作。<br>如今，宜蘭一年一度的端午龍舟賽、國際童玩節都在親水公園舉行；農曆七夕情人節還搭起鵲橋，增添浪漫氣氛。近幾年來，親水公園已成為臺灣旅遊觀光最HOT的景點之一了。"
 }
 function changeDramaPavilion() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-宜蘭交流道下-縣道191線-縣民大道二段-中山路二段-復興路一段至二段。<br><br>大眾運輸：<br>搭臺鐵至宜蘭站下，轉搭葛瑪蘭客運771（往金六結）至文化中心站下。";
     img.src = "/view/g.jpg"
     title.innerHTML = "臺灣戲劇館"
     text.innerHTML = "位於宜蘭縣政府文化局內，此館以收藏歌仔戲相關事務為主、傀儡戲為輔的「臺灣戲劇中心」，館內收藏豐富，對於臺灣民間戲劇、地方音樂的演變、保存、研究及推廣，做了詳盡的介紹。館內並設有透明片文物區、傳統戲臺及本地和現代歌仔戲劇場的模型，並有視廳室、曲調介紹區，讓參觀者透過不同方式輕易瞭解歌仔戲。</br><h3>歌仔戲</h3></br>以民謠小調為基礎，並吸收其他劇種的音樂、身段、服飾、道具而形成的閩南語歌唱劇，是臺灣最受歡迎的傳統戲劇。</br><h3>傀儡戲</h3></br>由人操作戲偶表演的戲劇形式，中國古代即已盛行，其種類多，臺灣則僅見懸絲傀儡一種。"
 }
 function changeRenshanBotanicalGarden() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-羅東交流道下-縣道191線-鄉道宜30線-丸山路-新寮路<br><br>大眾運輸：<br>搭臺鐵至羅東站下-轉搭首都客運281至仁山植物園站下。";
     img.src = "/view/h.jpg"
     title.innerHTML = "仁山植物園"
     text.innerHTML = "仁山植物園位於中央山脈的最北端，海拔高度約50至500公尺，面積約有100公頃，屬臺灣低海拔亞熱帶東北季風雨林型的氣候；北邊面向蘭陽平原及太平洋，園區後面則依傍著－新寮山及新舊寮瀑布。<br>由於園區距離市區不遠，交通方便，並建設有不錯的登山及休憩設施，時常有民眾到此登山健行。<br>宜蘭縣政府為有效建立園區多元化功能，自民國90年（西元2001年）開始積極規劃園區，期使能成為兼具休閒、生活、生態以及教育的一個植物園。"
 }
 function changeservice() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-羅東交流道下-縣道191線-鄉道宜12線-省道臺2線。<br><br>大眾運輸：<br>1.搭高鐵或臺鐵至臺北站下-轉搭國光客運1812（往南方澳）至壯圍沙丘旅遊服務園區站下。<br>2.搭臺鐵至宜蘭站下-轉搭國光客運1787、1788至壯圍沙丘旅遊服務園區站下。<br>3.搭臺鐵至宜蘭站下-轉搭台灣好行綠18壯圍沙丘線至壯圍沙丘旅遊服務園區站下。<br><br>無障礙交通資訊<br><br>大眾運輸︰<br>搭臺鐵至宜蘭站下-轉搭台灣好行綠18壯圍沙丘線（低地板）至壯圍沙丘旅遊服務園區站下。<br>復康巴士︰宜蘭縣政府社會處";
     img.src = "/view/i.jpg"
     title.innerHTML = "壯圍沙丘旅遊服務園區"
     text.innerHTML = "壯圍沙丘旅遊服務園區位於臺2線濱海公路149.3K旁，面積約4.8公頃，為宜蘭國際魅力新景點。園區由名建築師黃聲遠，以「空出谷地，看見沙丘」為核心概念規劃設計，融合宜蘭地方自然元素呈現沙丘地景風貌及沙穴建築空間營造，走在園區一旁是寧靜的鄉村田野，一旁是池水倒映著天氣變化和綿延的林投樹樹梢，不但景致襯托出壯圍沙丘旅遊服務園區的美景，眼前起伏廣大沙丘景色，讓人不自覺地會放慢腳步。<br>園區於民國107年（西元2018年）4月28日落成啟用，規劃有遊客中心、沙丘展覽館（由蔡明亮導演策劃）及森林島嶼（由薰衣草森林進駐經營）等空間。另外周邊還有壯圍海濱、自行車道、後埤及廍後特色社區、蘭陽溪口賞鳥保護區等特色美景，不論在晨曦，日落，還有月昇時刻，漫步園區、聆聽浪潮的聲音、賞景休憩都是絕佳享受，來到這裡可以放慢腳步，細細品味這座沙丘建築，牽引出恬靜自然及人文風情。"
 }
 function changespapark() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-頭城交流道下-青雲路一段-礁溪路七段至六段-公園路。<br><br>大眾運輸：<br>搭臺鐵至礁溪站下。";
     img.src = "/view/j.jpg"
     title.innerHTML = "礁溪溫泉公園"
     text.innerHTML = "礁溪溫泉公園位於火車站北方約500公尺處，園內林蔭夾道，為民眾休閒之處。礁溪鄉以溫泉聞名，礁溪溫泉屬於碳酸氫鈉泉，富含鈉、鎂、鈣、鉀、碳酸離子等化學成份，是臺灣少見的平地溫泉，不論是浸泡、沐浴或經處理後成為礦泉水飲用，都對身體健康有幫助。<br>公園內建造一處可容納約200人的日本和風式泡湯池，並設有旅遊服務中心，提供來到宜蘭的旅客縣內相關旅遊觀光資訊諮詢服務，讓民眾一年四季都能享受溫泉洗浴的樂趣。"
 }
 function changeforestry() {
-    let img = document.getElementById('picture')
-    let title = document.getElementById('title')
-    let text = document.getElementById('textarea')
+    let img = document.getElementById('picture');
+    let title = document.getElementById('title');
+    let text = document.getElementById('textarea');
+    document.getElementById("mapword").innerHTML = "<br>自行開車：<br>國道5號-羅東交流道下-縣道191線-縣道196線-中正北路。<br><br>大眾運輸：<br>搭臺鐵至羅東站下-轉搭台灣好行綠21冬山河線至羅東林業文化園區站下。";
     img.src = "/view/k.jpg"
     title.innerHTML = "羅東林業文化區"
     text.innerHTML = "羅東林業文化園區昔日為羅東出張所及貯木池，八仙山、阿里山以及太平山三大林場的筏木，皆運用至此存放。<br>園區坐落於羅東市區，佔地約16公頃，交通十分方便，園內規劃有貯木池、水生植物池、森林鐵道、竹林車站、蒸汽火車頭、文化展示館、茂密綠林和環池步道等，結合休閒、教育、文化的功能，是生態觀察的好去處。"

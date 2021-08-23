cfe6219c0a25ebf1814e3879106eca0d145edcde
funiro720720 <theend1010321@gmail.com>
2021-08-23 22:37:29 +0800

    新增隱藏視窗

---
+++ js/video/video2.js
@@ -0,0 +1,39 @@
+
+function btnFunction1() {
+    document.getElementById(
+        "container_front").style.display = "none";
+        document.getElementById(
+            "view").style.display = "block";
+}
+function back1() {
+    document.getElementById(
+        "container_front").style.display = "block";
+        document.getElementById(
+            "view").style.display = "none";
+}
+function btnFunction2() {
+    document.getElementById(
+        "container_front").style.display = "none";
+        document.getElementById(
+            "culture").style.display = "block";
+}
+function back2() {
+    document.getElementById(
+        "container_front").style.display = "block";
+        document.getElementById(
+            "culture").style.display = "none";
+}
+
+function btnFunction3() {
+    document.getElementById(
+        "container_front").style.display = "none";
+        document.getElementById(
+            "play").style.display = "block";
+}
+function back3() {
+    document.getElementById(
+        "container_front").style.display = "block";
+        document.getElementById(
+            "play").style.display = "none";
+}
+

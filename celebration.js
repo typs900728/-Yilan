41796a258d8546d69d49f220c189b75e4f45ddce
RDLK0712 <zxcnmijklpoq@gmai.com>
2021-08-27 19:15:54 +0800

    慶典

--- celebration.js
+++ celebration.js
@@ -1,49 +1,93 @@
-var Expand = (function() {
-  var tile = $('.strips__strip');
-  var tileLink = $('.strips__strip > .strip__content');
-  var tileText = tileLink.find('.strip__inner-text');
-  var stripClose = $('.strip__close');
-  
-  var expanded  = false;
+$(document).ready(function(){
 
-  var open = function() {
-      
-    var tile = $(this).parent();
-
-      if (!expanded) {
-        tile.addClass('strips__strip--expanded');
-        // add delay to inner text
-        tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
-        stripClose.addClass('strip__close--show');
-        stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
-        expanded = true;
-      } 
-    };
-  
-  var close = function() {
-    if (expanded) {
-      tile.removeClass('strips__strip--expanded');
-      // remove delay from inner text
-      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
-      stripClose.removeClass('strip__close--show');
-      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
-      expanded = false;
-    }
-  }
-
-    var bindActions = function() {
-      tileLink.on('click', open);
-      stripClose.on('click', close);
-    };
-
-    var init = function() {
-      bindActions();
-    };
-
-    return {
-      init: init
-    };
-
-  }());
-
-Expand.init();
+      $("button.btn-1").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/利澤簡.jpg")
+            $(".text-big").html("利澤簡走尪")
+            $(".text-mid").html("月份 : 2月&emsp;地點 : 五結鄉")
+            $(".text-small").html("有一年，利澤簡鬧瘟疫，當地仕紳決定擴大「弄尪」的範圍，並由永安宮主辦神明繞境巡庄及抬轎過火競賽的儀式，俗稱「走尪」，居民咸相信這項風俗可去穢治邪，保佑平安，因而傳承不墜。</br>「走尪」的方式就是把各庄頭的神明恭請在神轎內，按照神轎大小，分成四人一組，按一般的比賽規則，在一百廿公尺的利澤老街上競走，每兩轎為一組，以三次競走為限，分出勝負，誰輸了，就排在路邊當啦啦隊。</br>鄉公所指出，每逢農曆春節，利澤簡地區就會由永安宮當主神，結合清水、利澤簡、成興、新店、加禮宛、下福、堤斗厝、埤仔尾八大庄的廟宇，從年初開始，抬神轎繞境，一直到農曆正月十五日才回永安宮並且由永安宮供奉的媽祖禮尚往來做東，向八大庄請來的神轎進行「走尪」民俗活動。</br>各廟宇都希望掙得面子，在輸人不輸陣的情況下，「走尪」競爭激烈，曾引發衝突，被迫停辦半個世紀，直到民國八十九年才恢復比賽，鄉公所為推展地方觀光產業，今年擴大舉辦，區分寺廟、公開及學生組，變成一個具當地特色的民俗文化活動。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-1
+       $("button.btn-2").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/綠色博覽會.jpg")
+            $(".text-big").html("綠色博覽會")
+            $(".text-mid").html("月份 : 3~5月&emsp;地點 : 武荖坑風景區或冬山河生態綠舟")
+            $(".text-small").html("愛護環境，人人有責，從行動中實踐環保，提倡生態環保、綠色能源、環境教育等理念，強調人與土地間的關係，透過永續經營、永續生活的想法，為地球盡一份心力。</br>宜蘭是綠色生活的領航者，透過每年舉辦綠色博覽會，將臺灣的綠色生活概念，傳播出去，從生活、環境、產業等各方面，慢慢改進，有著與土地間共有、共享、共存的想法，讓生活中，都充滿綠色的能量。</br>乘著綠色方舟，航向嶄新的未來，宜蘭綠色博覽會，帶你理解綠色概念生活，如何落實在生活當中，將綠色的漣漪一層一層的擴散出去，期望全球都生活在綠色的環境中。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-2
+       $("button.btn-3").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/龍舟競渡.jpg")
+            $(".text-big").html("二龍競渡")
+            $(".text-mid").html("月份 : 6月&emsp;地點 : 礁溪鄉")
+            $(".text-small").html("相傳早年噶瑪蘭平埔族在二龍河沿岸的淇武蘭定居後，與居住在上游的番割田族，每年會定期舉辦划船比賽，採用木板舢或是紅頭渡船，嬉戲遊玩好不熱鬧。</br>然而淇武蘭河沙淤積越發嚴重，比賽地點也往下游遷移，演變成淇武蘭和洲仔尾，兩村間的競賽。</br>兩百多年來，二龍河的淇武蘭港，與當地居民生活息息相關，然而當地常發生溺水意外，村民以競渡方式，驅逐水中妖怪。</br>每到端午節，精緻的龍船，在河上爭先恐後地往終點前進，現場沒有裁判、計時員，輸贏全由隊員觀眾決定，賽情激烈、神聖又不凡，是全臺最獨特的民俗活動。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-3
+       $("button.btn-4").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/宜蘭童玩節.jpg")
+            $(".text-big").html("宜蘭國際童玩節")
+            $(".text-mid").html("月份 : 7~8月&emsp;地點 : 冬山河親水公園")
+            $(".text-small").html("炎炎夏日，到底要去哪裡玩呢？走到哪裡都好熱啊，揮之不去的暑氣壟罩而下，消暑最佳利器到底在哪裡呢？那就來宜蘭尋寶吧！</br>夏天玩水真的很過癮，大朋友小朋友最愛的宜蘭國際童玩節，在每年的夏季舉辦，現場擠滿玩水的人潮，滑水道、戲水池、噴泉等遊樂設施，讓來訪的遊客，玩的不亦樂乎。</br>放開心境，讓自己當小孩一次，盡情的遊玩，宜蘭童玩節，就是要讓你「童」玩一夏，帶著家人小孩一同玩水，不分大小，不分年紀，大家互相潑水嬉戲，開心不已。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-4
+       $("button.btn-5").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/蘇澳冷泉嘉年華.jpg")
+            $(".text-big").html("蘇澳冷泉嘉年華")
+            $(".text-mid").html("月份 : 7月&emsp;地點 : 蘇澳鎮")
+            $(".text-small").html("蘇澳有著世界知名的碳酸氫鈉鈣冷泉，盛夏時分，浸泡在冰涼的泉水中，暑氣瞬間消散。</br>體驗來自從天而降的水珠，與街道旁的民眾一同歡慶，蘇澳冷泉嘉年華，潑水狂歡，街頭大戰，有萬人踩街的盛況。</br>創意踩街、花車遊行、街頭表演等創意的狂歡活動，讓蘇澳湧入數萬遊客，拿起水球互丟、潑水，各個展現許久沒發揮的身手。</br>品嘗蘇澳冷泉製作的Q彈羊羹，甜蜜蜜幸福滋味，在口中散發出來，再來口特製的冷泉冰棒，獨特的口感，讓人回味無窮，蘇澳冷泉的神秘力量，你心動了嗎？")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-5
+       $("button.btn-6").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/頭城搶孤.jpg")
+            $(".text-big").html("頭城搶孤")
+            $(".text-mid").html("月份 : 8~9月&emsp;地點 : 頭城鎮")
+            $(".text-small").html("早年漢人來到宜蘭開疆闢土，經歷天災、戰爭、疾病等磨難，讓往生者無所依歸，在每年農曆七月，居民聯合舉辦普渡儀式，消災解厄、普渡孤魂，並在農曆鬼門關之夜，展開精彩萬分的搶孤儀式。</br>一年一度的頭城搶孤競賽，在子時開演，參賽者攀爬在塗滿牛油的棚柱上，一步一步地往上攀爬，感受來自團隊互助合作的精神，過程激烈驚險，讓搶孤的參與者，體驗搶救孤魂、普渡野鬼的艱辛歷程。</br>有著宗教、歷史、文化內涵的頭城搶孤，吸引來自世界各地的遊客參與，現場氣氛熱鬧滾滾，目睹年度盛大的宗教慶典。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-6
+       $("button.btn-7").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/不老節.jpg")
+            $(".text-big").html("不老節")
+            $(".text-mid").html("月份 : 10月&emsp;地點 : 宜蘭市或羅東鎮")
+            $(".text-small").html("宜蘭不老節是全國第一個為長者舉辦的活動，打破老人就是活力低、走不動的型態，是銀髮族的活動樂園，讓長者保有人老心不老的心境，展現出熱血活力、精力無限的動感老人。</br>動動手啊，動動腳啊，扭扭腰啊，轉個圈，長者跟著音樂揮舞手腳，每個人臉上笑口常開，賣力地唱唱跳跳，開心不已。</br>隨著經典老歌的撥放，讓長者彷彿回到青春歲月，回味早年生活的歡樂時光，展現生命的熱情，引吭高歌的長者，唱出心中滿滿的感動。</br>不老節為銀髮族敞開大門，開啟活力的嶄新世界，營造友善的高齡城市，讓長者互相交流、彼此關照，聊天、唱歌、跳舞等系列活動，開啟全新的生命價值。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-7
+       $("button.btn-8").click(function(){
+        $("div.content").fadeOut(1000,function(){
+            $(".picture").attr("src","celebration-picture/二結王過火.jpg")
+            $(".text-big").html("二結王公過火")
+            $(".text-mid").html("月份 : 12~1月&emsp;地點 : 五結鄉")
+            $(".text-small").html("二結王公廟供奉的祀神為「古公三王」，相傳南宋末年，有三位義士抵抗蒙古兵馬，最後壯烈犧牲，鄉民感念三位的忠義，建寺廟宇，尊稱他們為「古公三王」。</br>每年農曆11月15日，會舉辦二結王公過火儀式，消災解厄、除晦氣、去霉運，是臺灣規模最盛大的「過炭火」民俗活動，已有百年之久，俗諺中有句：「驚王公生，不驚過年」，彰顯出當地住民對於祭典的重視。</br>在耆老帶領之下，將兩萬餘斤的炭火堆疊成小山，時程一到，工作人員往火堆中灑鹽米，有著去除不潔之意，並在萬眾矚目下，信徒抬著武轎，依序走過火堆，廟裡廟外熱鬧萬分，</br>民俗的信仰是在地最珍貴的文化，療癒參與者的心靈，「二結王公過火」在2007年，被國家文化總會評選文臺灣10大民俗祭典。")
+        })
+        $("div.content").fadeIn(1000)
+       });
+// btn-8
+        $("button.btn-9").click(function(){
+            $("div.content").fadeOut(1000,function(){
+                $(".picture").attr("src","celebration-picture/冬戀蘭陽溫泉季.jpg")
+                $(".text-big").html("礁溪溫泉季")
+                $(".text-mid").html("月份 : 12月&emsp;地點 : 礁溪鄉")
+                $(".text-small").html("喧鬧的車水馬龍，煩悶的工作瑣事，您累了嗎?想不想來一趟身、心、靈都放鬆的旅程呢?</br>一年一度由宜蘭縣舉辦的礁溪溫泉季就是您最好的選擇，提供健康、休閒、養生之氛圍外，還有樂團、舞團以及街頭藝人的精彩表演，一趟礁溪溫泉之旅，吃、喝、玩、樂，應有盡有。</br>聲名遠播的「冬戀宜蘭溫泉季」，在2016年更名為「礁溪溫泉季」，將樂舞活動融入活動當中，呈現出熱鬧的節慶氛圍，將礁溪獨特的溫泉，廣為傳播，讓來訪的遊客，認識礁溪、認識宜蘭、認識夢幻的溫泉之鄉。")
+            })
+            $("div.content").fadeIn(1000)
+        });
+// btn-9 
+});      
